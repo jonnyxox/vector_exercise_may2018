a218ce1e5a1ebcae9ec3f57c6fd5c8a1a6a196e7
Tristan Brindle <t.c.brindle@gmail.com>
2018-06-19 19:15:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# C++ London Uni Vector Exercise #
+
+This is a short exercise to get you familiar with using `std::vector`.
+
+The file `main.cpp` contains a list of instructions for you to follow.
+Make sure that you understand what each `vector` member function is doing.
+
+If you need help, you can find reference documentation for `vector`
+[on cppreference.com](http://en.cppreference.com/w/cpp/container/vector),
+or lots more material in chapter 18 of the textbook.
---
+++ main.cpp
@@ -0,0 +1,36 @@
+
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    // 1. Create a vector of ints containing elements 1, 2, 3
+
+    // 2. Use assert() to check that the vector contains three elements
+
+    // 3. Add two more elements to the back of the array
+    //    What is its new capacity?
+
+    // 4. Remove the last element from the array
+    //    What is its size?
+    //    What is its capacity?
+
+    // 5. Use a range-for loop to set every element of the vector to 42
+
+    // 6. Set the second element of the vector to 99
+    //    Hint: remember C++ indexes arrays from zero!
+
+    // 7. Use a range-for loop to print every element of the function
+    //    For bonus points: make this a separate function called print_vec()
+
+    // 8. Use a single member function to remove all elements from the vector
+    //    Use assert() to check that this has worked correctly
+
+    // 9. What is the capacity of your vector now?
+    //    Use a member function to reset the capacity to zero
+
+    // 10. Try reading the 99th element of the vector. What happens?
+    //     Now, try using the at(idx) member function to read the 99th element.
+    //     What happens this time?
+}
